313b4494c0ba625f1b59ab98fbea2fe4387034ad
Vinicius Cestari <viniciuscestari01@gmail.com>
2024-11-12 21:59:31 -0300

    feat: add copy from click

--- src/lib/components/clipboard-entry.svelte
+++ src/lib/components/clipboard-entry.svelte
@@ -3,24 +3,35 @@
 	import type { ClipboardEntry } from '$lib/types';
 	import ClipboardTypeBadge from './clipboard-type-badge.svelte';
 	import ClipboardWindowBadge from './clipboard-window-badge.svelte';
+	import { writeText } from '@tauri-apps/plugin-clipboard-manager';
+
+	async function handleEntryClick(text: string) {
+  await writeText(text);
+  alert("Text copied! Use Ctrl+V (or Command+V) to paste it in the desired location.");
+}
 
 	let { entry }: { entry: ClipboardEntry } = $props();
 </script>
 
-<li class="card bg-base-300 p-2 h-32 flex flex-col justify-between">
-	<ClipboardWindowBadge window={entry.window} />
-	<!--- add ability to hide --->
-	<p
-		style="-webkit-line-clamp: 3; -webkit-box-orient: vertical; display: -webkit-box;"
-		class="break-words text-ellipsis overflow-hidden text-sm pl-1"
+<li>
+	<button
+		class="btn p-2 h-36 flex flex-col flex-nowrap items-start justify-between w-full text-start"
+		onclick={() => handleEntryClick(entry.text)}
 	>
-		{entry.text}
-	</p>
-	<div class="card-actions justify-between items-center">
-		<ClipboardTypeBadge type={entry.type} />
+		<ClipboardWindowBadge window={entry.window} />
 		<!--- add ability to hide --->
+		<p
+			style="-webkit-line-clamp: 3; -webkit-box-orient: vertical; display: -webkit-box;"
+			class="break-words text-ellipsis overflow-hidden text-sm pl-1"
+		>
+			{entry.text}
+		</p>
+		<div class="card-actions justify-between items-center">
+			<ClipboardTypeBadge type={entry.type} />
+			<!--- add ability to hide --->
 
-		<time class="text-sm">{formatDate(entry.timestamp)}</time>
-		<!--- add ability to hide --->
-	</div>
+			<time class="text-sm">{formatDate(entry.timestamp)}</time>
+			<!--- add ability to hide --->
+		</div>
+	</button>
 </li>
