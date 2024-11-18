05b21e1ce45d04c1288e1ea0e30c9d646418cf41
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-11-18 11:40:24 -0300

    feat: add toggle input to change entry visualization

--- src/routes/configuration/+page.svelte
+++ src/routes/configuration/+page.svelte
@@ -2,6 +2,8 @@
 	import ClipboardEntry from '$lib/components/clipboard-entry.svelte';
 	import type { ClipboardEntry as ClipboardEntryType } from '$lib/types';
 	import ThemeToggle from '$lib/components/theme-toggle.svelte';
+	import ClipboardWindowBadge from '$lib/components/clipboard-window-badge.svelte';
+	import ClipboardTypeBadge from '$lib/components/clipboard-type-badge.svelte';
 
 	const entry1: ClipboardEntryType = {
 		id: Infinity,
@@ -20,13 +22,28 @@
 	};
 </script>
 
-<div class="grid sm:grid-cols-4">
+<div class="grid xs:grid-cols-[minmax(0,_12rem)_minmax(0,_1fr)] xs:gap-4">
 	<div class="flex flex-col pt-1">
 		<ThemeToggle />
+		<div class="form-control">
+			<div class="divider text-sm">Show</div>
+			<label class="label cursor-pointer">
+				<span class="label-text"><ClipboardWindowBadge window="Window" /></span>
+				<input type="checkbox" class="toggle toggle-primary" />
+			</label>
+			<label class="label cursor-pointer">
+				<span class="label-text"><ClipboardTypeBadge type="text" /></span>
+				<input type="checkbox" class="toggle toggle-primary" />
+			</label>
+			<label class="label cursor-pointer">
+				<span class="label-text">Time</span>
+				<input type="checkbox" class="toggle toggle-primary" />
+			</label>
+		</div>
 	</div>
 
-	<div class="col-span-3">
-		<div class="divider sm:divider-horizontal text-sm">Preview</div>
+	<div>
+		<div class="divider xs:divider-horizontal text-sm">Preview</div>
 		<ClipboardEntry entry={entry1} />
 		<ClipboardEntry entry={entry2} />
 	</div>
