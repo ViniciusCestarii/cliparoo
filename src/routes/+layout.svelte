1400b6f5a7a780e7637977caacac1b2e84b92f04
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-11-18 16:22:53 -0300

    feat: add interval for copy check on layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
 	import Header from '$lib/components/header.svelte';
 	import '../app.css';
+	import { invokeGetWindowName } from '$lib';
+	import { readText } from '@tauri-apps/plugin-clipboard-manager';
+	import cs from '$lib/cliparoo-state.svelte';
+	
 	let { children } = $props();
+
+	// fix:
+	// [2024-11-09][23:47:40][tao::platform_impl::platform::event_loop::runner][WARN] NewEvents emitted without explicit RedrawEventsCleared
+	// [2024-11-09][23:47:40][tao::platform_impl::platform::event_loop::runner][WARN] RedrawEventsCleared emitted without explicit MainEventsCleared
+	$effect(() => {
+		const id = setInterval(async () => {
+			const [clipboardContent, currentWindowName] = await Promise.all([
+				await readText(),
+				await invokeGetWindowName()
+			]);
+
+			const createdEntry = cs.addClipboardEntry({
+				text: clipboardContent,
+				window: currentWindowName
+			});
+
+			if (createdEntry) {
+				window.scrollTo({ top: 0, behavior: 'smooth' });
+			}
+		}, 200);
+
+		return () => {
+			clearInterval(id);
+		};
+	});
 </script>
 
 <Header />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,7 @@
 <script lang="ts">
-	import { invokeGetWindowName } from '$lib';
 	import cs from '$lib/cliparoo-state.svelte';
 	import ClipboardEntry from '$lib/components/clipboard-entry.svelte';
 	import VirtualList from '$lib/components/virtual-list.svelte';
-	import { readText } from '@tauri-apps/plugin-clipboard-manager';
-
-	// fix:
-	// [2024-11-09][23:47:40][tao::platform_impl::platform::event_loop::runner][WARN] NewEvents emitted without explicit RedrawEventsCleared
-	// [2024-11-09][23:47:40][tao::platform_impl::platform::event_loop::runner][WARN] RedrawEventsCleared emitted without explicit MainEventsCleared
-	$effect(() => {
-		const id = setInterval(async () => {
-			const [clipboardContent, currentWindowName] = await Promise.all([
-				await readText(),
-				await invokeGetWindowName()
-			]);
-
-			const createdEntry = cs.addClipboardEntry({
-				text: clipboardContent,
-				window: currentWindowName
-			});
-
-			if (createdEntry) {
-				window.scrollTo({ top: 0, behavior: 'smooth' });
-			}
-		}, 200);
-
-		return () => {
-			clearInterval(id);
-		};
-	});
 </script>
 
 <div class="h-[calc(100vh-4rem)]">
