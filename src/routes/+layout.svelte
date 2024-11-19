ff451a211fda8c1c80556d4922fbfea181ce87cd
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-11-19 10:24:22 -0300

    feat: add raw write image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import Header from '$lib/components/header.svelte';
 	import '../app.css';
 	import { invokeGetWindowName } from '$lib';
-	import { readText } from '@tauri-apps/plugin-clipboard-manager';
+	import { readText, readImage } from '@tauri-apps/plugin-clipboard-manager';
 	import cs from '$lib/cliparoo-state.svelte';
 
 	let { children } = $props();
@@ -12,18 +12,49 @@
 	// [2024-11-09][23:47:40][tao::platform_impl::platform::event_loop::runner][WARN] RedrawEventsCleared emitted without explicit MainEventsCleared
 	$effect(() => {
 		const id = setInterval(async () => {
-			const [clipboardContent, currentWindowName] = await Promise.all([
-				await readText(),
-				await invokeGetWindowName()
-			]);
+			try {
+				const [clipboardText, currentWindowName] = await Promise.all([
+					readText(),
+					invokeGetWindowName()
+				]);
 
-			const createdEntry = cs.addClipboardEntry({
-				text: clipboardContent,
-				window: currentWindowName
-			});
+				const createdEntry = cs.addClipboardEntry({
+					text: clipboardText,
+					window: currentWindowName
+				});
 
-			if (createdEntry) {
-				window.scrollTo({ top: 0, behavior: 'smooth' });
+				if (createdEntry) {
+					window.scrollTo({ top: 0, behavior: 'smooth' });
+				}
+			} catch (error) {
+				function toBase64(imageBlob: Blob) {
+					return new Promise((resolve, reject) => {
+						const reader = new FileReader();
+						reader.onloadend = () => resolve(reader.result); // This gives the base64 string
+						reader.onerror = reject;
+						reader.readAsDataURL(imageBlob); // Read the Blob as base64
+					});
+				}
+
+				// Assuming `clipboardImage` is a Blob or Image object
+				const [clipboardImage, currentWindowName] = await Promise.all([
+					readImage(), // This should return a Blob or Image object
+					invokeGetWindowName()
+				]);
+				
+				const blob = new Blob([await clipboardImage.rgba()], { type: 'image' })
+
+				const imageBase64 = await toBase64(blob); // Convert the image to base64
+
+				// Add clipboard entry with base64 image
+				const createdEntry = cs.addClipboardEntry({
+					text: imageBase64,
+					window: currentWindowName,
+				});
+
+				if (createdEntry) {
+					window.scrollTo({ top: 0, behavior: 'smooth' });
+				}
 			}
 		}, 200);
 
