0321a21a690addd7895edfb20e11623f89c69aab
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-11-25 12:51:51 -0300

    feat: componentify entries and remove useless deps

---
+++ src/lib/components/example/clipboard-entry-1.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import ClipboardEntry from "../clipboard-entry.svelte";
+  import type { ClipboardEntry as ClipboardEntryType } from '$lib/types';
+
+	const entry: ClipboardEntryType = {
+		id: Infinity,
+		text: 'https://github.com/ViniciusCestarii/cliparoo',
+		window: 'Cliparoo',
+		timestamp: new Date().toISOString(),
+		type: 'url'
+	};
+</script>
+
+<ClipboardEntry entry={entry} />
