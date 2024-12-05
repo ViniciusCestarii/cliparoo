8d309c8240ace49016e7293affe94ba03a72de97
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-12-05 13:35:47 -0300

    feat: improve search feature adding keyword filters and help button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,62 @@
 	import { focus } from '$lib/actions.svelte';
 	import cs from '$lib/cliparoo-state.svelte';
 	import ClipboardEntry from '$lib/components/clipboard-entry.svelte';
+	import SearchFilterHelp from '$lib/components/search-filter-help.svelte';
 	import VirtualList from '$lib/components/virtual-list.svelte';
 	import { cn } from '$lib/utils';
 
 	let search = $state('');
+	let searchBlock: HTMLElement | null = $state(null);
 
 	let displaySearch = $state(false);
+
+	function parseFilter(filterStr: string) {
+		// Ensure values with spaces are quoted
+		filterStr = filterStr.replace(/(\w+):([^"\s]+)(?=\s|$)/g, '$1:"$2"'); // Add quotes to non-quoted values with spaces
+
+		const filters: Record<string, string> = {};
+		const regex = /(\w+):"([^"]*)"/g;
+		let match;
+
+		while ((match = regex.exec(filterStr)) !== null) {
+			const key = match[1];
+			const value = match[2];
+			filters[key] = value;
+		}
+
+		return filters;
+	}
 
 	let filteredClipboard = $derived.by(() => {
 		const upperSearch = search.toUpperCase();
 
+		const isNoFilter = upperSearch.startsWith('NOFILTER');
+
+		const adjustedSearch = isNoFilter ? upperSearch.slice(8) : upperSearch;
+
+		const filters = isNoFilter ? {} : parseFilter(adjustedSearch);
+
+		if (Object.keys(filters).length > 0) {
+			return cs.clipboard.filter((entry) => {
+				return Object.entries(filters).every(([key, value]) => {
+					if (key === 'TYPE') {
+						return entry.type.toUpperCase().includes(value);
+					}
+
+					if (key === 'WINDOW') {
+						return entry.window.toUpperCase().includes(value);
+					}
+
+					return entry.text.toUpperCase().includes(value);
+				});
+			});
+		}
+
 		return cs.clipboard.filter(
 			(entry) =>
-				entry.text.toUpperCase().includes(upperSearch) ||
-				entry.window.toUpperCase().includes(upperSearch) ||
-				entry.type.toUpperCase().includes(upperSearch)
+				entry.text.toUpperCase().includes(adjustedSearch) ||
+				entry.window.toUpperCase().includes(adjustedSearch) ||
+				entry.type.toUpperCase().includes(adjustedSearch)
 		);
 	});
 
@@ -27,24 +68,36 @@
 		}
 	}
 
+	function handleClickOutside(event: MouseEvent) {
+		if (search) return;
+		if (searchBlock && !searchBlock.contains(event.target as Node)) {
+			console.log(searchBlock);
+			displaySearch = false;
+		}
+	}
+
 	$effect(() => {
 		window.addEventListener('keydown', handleKeydown);
+		document.addEventListener('click', handleClickOutside);
 
 		return () => {
 			window.removeEventListener('keydown', handleKeydown);
+			document.removeEventListener('click', handleClickOutside);
 		};
 	});
 </script>
 
 {#if displaySearch}
-	<div class="flex px-2 h-12 mb-1">
+	<div bind:this={searchBlock} class="flex px-2 h-12 mb-1 relative">
 		<input
 			use:focus
-			onblur={() => (displaySearch = false)}
 			placeholder="Search clipboard"
-			class="w-full bg-base-100 px-2 outline-none rounded-btn rounded-t-none focus-within:ring-2 ring-accent/70"
+			class="w-full bg-base-100 px-2 outline-none rounded-btn rounded-t-none ring-2 ring-accent/70"
 			bind:value={search}
 		/>
+		<div class="absolute right-2">
+			<SearchFilterHelp class="bg-base-100 hover:bg-base-300" />
+		</div>
 	</div>
 {/if}
 
